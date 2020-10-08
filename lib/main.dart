719e11a8dbe692e32ee8c6615d237799694ca299
Mert Ozdil <merttozdill@gmail.com>
2020-10-08 18:53:10 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'dart:convert';
+void main() => runApp(new MaterialApp(
+  theme: new ThemeData(
+    primarySwatch: Colors.teal
+  ),
+  home: new HomePage(),
+));
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List data;
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("Load Json App"),
+      ),
+      body: new Container(
+        child: new Center(
+          child: new FutureBuilder(
+            future: DefaultAssetBundle
+                .of(context)
+                .loadString('load_json/person.json'),
+              builder: (context, snapshot){
+              //DECODE JSON
+                var mydata = json.decode(snapshot.data.toString());
+                return new ListView.builder(
+                    itemBuilder: (BuildContext context, int index){
+                      return new Card(
+                        child: new Column(
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                          children: <Widget>[
+                            new Text("Name : "+mydata[index]['name']),
+                            new Text("Age : "+mydata[index]['age']),
+                            new Text("Height : "+mydata[index]['height']),
+                            new Text("Hair Color : "+mydata[index]['hair_color']),
+                            new Text("Gender : "+mydata[index]['gender']),
+                          ],
+                        ),
+                      );
+                    },
+                  itemCount: mydata == null ? 0 : mydata.length,
+                );
+              }
+          )
+        ),
+      ),
+    );
+  }
+}
